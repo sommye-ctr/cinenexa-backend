88f54f2dc6ed165a1ed9b6b2d3532fc338a9c036
Somye <mahajan.somye@gmail.com>
2024-12-12 19:09:10 +0530

    Initial commit

---
+++ src/main/java/com/cinenexa/cinenexabackend/CinenexaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cinenexa.cinenexabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinenexaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinenexaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cinenexa/cinenexabackend/CinenexaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinenexa.cinenexabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinenexaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
